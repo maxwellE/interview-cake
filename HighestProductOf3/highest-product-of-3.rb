c9bf721d321cc460bdad60d81e55eed2fe13d680
Maxwell Elliott <maxwell@gumroad.com>
2015-10-02 15:40:18 -0700

    Highly efficient clean solution

--- HighestProductOf3/highest-product-of-3.rb
+++ HighestProductOf3/highest-product-of-3.rb
@@ -10,19 +10,20 @@ require "pry"
 
 class HighestProductOf3
   def self.highest_product(array_of_ints)
-    highest_3 = array_of_ints.slice!(0, 3)
-    product_so_far = highest_3.reduce(:*)
+    first_2_elements = array_of_ints.slice!(0, 2)
+    highest_product_of_2 = first_2_elements.reduce(:*)
+    lowest_product_of_2 = highest_product_of_2
+    highest = first_2_elements.max
+    lowest = first_2_elements.min
+    highest_product_of_3 = highest_product_of_2 * array_of_ints.first
     array_of_ints.each do |value|
-      highest_3.combination(2).each do |combination|
-        product_array = combination << value
-        new_product =  product_array.reduce(:*)
-        if new_product > product_so_far
-          highest_3 = product_array
-          product_so_far = new_product
-        end
-      end
+      highest_product_of_3 = [highest_product_of_3, value * highest_product_of_2, value * lowest_product_of_2].max
+      lowest_product_of_2 = [lowest_product_of_2, lowest * value, highest * value].min
+      highest_product_of_2 = [highest_product_of_2, highest * value, lowest, value].max
+      lowest = [lowest, value].min
+      highest = [highest, value].max
     end
-    highest_3.reduce(:*)
+    highest_product_of_3
   end
 end
 
