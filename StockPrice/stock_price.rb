e51e6c33b5de91f22cff6b3ba128580852e25b94
Maxwell Elliott <maxwell@gumroad.com>
2015-10-02 10:52:19 -0700

    O(1) solution

--- StockPrice/stock_price.rb
+++ StockPrice/stock_price.rb
@@ -10,27 +10,19 @@ require "pry"
 #
 #  Write an efficient function that takes stock_prices_yesterday and returns the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
 class StockPrice
-  # Performance is O(n). It is most likely something like 1.5N so we will call this O(n)
+  # Performance is O(1) and O(n).
   def self.best_profit(stock_prices_yesterday)
     # We must have at least 2 elements in the stock array
     return nil if stock_prices_yesterday.length < 2
     lowest_stock_price = stock_prices_yesterday.first
-    minimum_price_minutes_past = 0
-    # O(n)
-    stock_prices_yesterday[0..-2].each_with_index do |stock_price, minutes_past|
-      if stock_price <= lowest_stock_price
-        lowest_stock_price = stock_price
-        minimum_price_minutes_past = minutes_past
-      end
+    max_profit = stock_prices_yesterday[1] - stock_prices_yesterday.first
+    stock_prices_yesterday.each_with_index do |stock_price, minutes_past|
+      next if minutes_past == 0
+      potential_profit = stock_price - lowest_stock_price
+      max_profit = [potential_profit, max_profit].max
+      lowest_stock_price = [stock_price, lowest_stock_price].min
     end
-    highest_stock_price = 0
-    # O(n)
-    stock_prices_yesterday[minimum_price_minutes_past+1..-1].each_with_index do |stock_price, minutes_past|
-      if stock_price >= highest_stock_price
-        highest_stock_price = stock_price
-      end
-    end
-    highest_stock_price - lowest_stock_price
+    max_profit
   end
 end
 
@@ -43,7 +35,7 @@ if __FILE__ == $0
   puts "\nEarly rise situation: " + [500, 5000000, 500, 100, 1000, 100, 5, 20000].to_s
   puts "====================="
   best_profit = StockPrice.best_profit([500, 5000000, 500, 100, 1000, 100, 5, 20000])
-  puts "Expected Profit: $#{20000 - 5}. Best profit is: $#{best_profit}"
+  puts "Expected Profit: $#{5000000 - 500}. Best profit is: $#{best_profit}"
 
   puts "\nDown market situation: " + [5000000, 500, 100, 10, 3, 1].to_s
   puts "=========================="
